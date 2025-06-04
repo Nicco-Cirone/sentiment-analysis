380e27064287ed4fe328641baaa14e63b52057a5
Nicco Cirone <nicco.cirone@gmail.com>
2025-06-04 13:32:11 +0000

    Delete classify.py

--- classify.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
-
-text = "I love DistilBERT"
-result = classifier(text)
-
-print("Text:", text)
-print("Prediction:", result)
